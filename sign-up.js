d667d383b75db1d3c609a169fee67a34b8d3031e
brennenmichael <kelyen@aol.com>
2022-03-20 20:54:43 -0400

    Password match error handling.

--- sign-up.js
+++ sign-up.js
@@ -1,5 +1,3 @@
-// There are many ways to pick a DOM node; here we get the form itself and the email
-// input box, as well as the span element into which we will place the error message.
 const form = document.getElementsByTagName("form")[0];
 
 const password = document.getElementById("password");
@@ -7,47 +5,37 @@ const confirmPassword = document.getElementById("confirm-password");
 const errorMessage = document.querySelector(".error-message");
 
 password.addEventListener("input", (e) => {
-  // Each time the user types something, we check if the
-  // form fields are valid.
-
-  if (password.value === confirmPassword.value) {
-    // In case there is an error message visible, if the field
-    // is valid, we remove the error message.
-    console.log("EQUAL");
-    errorMessage.textContent = ""; // Reset the content of the message
-    errorMessage.className = "error"; // Reset the visual state of the message
+  if (password.value === confirmPassword.value && password.value !== "") {
+    errorMessage.textContent = "";
+    errorMessage.classList.remove("active");
+    password.classList.remove("error");
+    confirmPassword.classList.remove("error");
   } else {
-    // If there is still an error, show the correct error
-    showError();
+    errorMessage.textContent = "*Passwords do not match.";
+    errorMessage.classList = "error-message active";
+    password.className = "error";
+    confirmPassword.className = "error";
   }
 });
 
-form.addEventListener("submit", function (event) {
+confirmPassword.addEventListener("input", (e) => {
+  if (password.value === confirmPassword.value && password.value !== "") {
+    errorMessage.textContent = "";
+    errorMessage.classList.remove("active");
+    password.classList.remove("error");
+    confirmPassword.classList.remove("error");
+  } else {
+    errorMessage.textContent = "*Passwords do not match.";
+    errorMessage.classList += " active";
+    password.className = "error";
+    confirmPassword.className = "error";
+  }
+});
+
+form.addEventListener("submit", (e) => {
   // if the email field is valid, we let the form submit
 
-  if (!email.validity.valid) {
-    // If it isn't, we display an appropriate error message
-    showError();
-    // Then we prevent the form from being sent by canceling the event
-    event.preventDefault();
+  if (password.value === confirmPassword.value) {
+    e.preventDefault();
   }
 });
-
-function showError() {
-  if (email.validity.valueMissing) {
-    // If the field is empty,
-    // display the following error message.
-    errorMessage.textContent = "You need to enter an e-mail address.";
-  } else if (email.validity.typeMismatch) {
-    // If the field doesn't contain an email address,
-    // display the following error message.
-    errorMessage.textContent = "Entered value needs to be an e-mail address.";
-  } else if (email.validity.tooShort) {
-    // If the data is too short,
-    // display the following error message.
-    errorMessage.textContent = `Email should be at least ${email.minLength} characters; you entered ${email.value.length}.`;
-  }
-
-  // Set the styling appropriately
-  errorMessage.className = "error active";
-}
